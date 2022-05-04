c44ea8d567997371f0215d4ddf78973604a79d64
Alejandro Escobar <alex89.desarrollo@gmail.com>
2022-05-03 20:29:09 -0400

    Minor changes.

--- src/stores/app.js
+++ src/stores/app.js
@@ -11,18 +11,30 @@ export const useAppStore = defineStore('app', {
     showError(error) {
       let msg = 'Unable to process your request';
 
-      if (error.response && error.response.data && error.response.data.message) {
-        msg = error.response.data.message;
+      try {
+        if (error.response && error.response.data && error.response.data.message) {
+          msg = error.response.data.message;
 
-        if (error.response.data.errors && (typeof error.response.data === 'object')) {
-          error.response.data.errors.map((value) => {
-            msg += `\n\n- ${value}`;
-          });
+          if (error.response.data.errors) {
+            const errors = error.response.data.errors;
+
+            if (Array.isArray(errors)) {
+              errors.map((error) => {
+                msg += `\n\n- ${error}`;
+              });
+            } else if (typeof errors === 'object') {
+              for (const error in errors) {
+                msg += '\n\n- ' + errors[error];
+              }
+            }
+          }
+        } else if (typeof error === 'object') {
+          msg = error.toString().replace(/^Error:\s/, '');
+        } else if (typeof error === 'string') {
+          msg = error;
         }
-      } else if (typeof error === 'object') {
-        msg = error.toString().replace(/^Error:\s/, '');
-      } else if (typeof error === 'string') {
-        msg = error;
+      } catch (e) {
+        msg += '\nPlease check the logs for more info.';
       }
 
       document.querySelector('#error-toast .toast-body').innerText = msg;
